78d8a250a3c98ff7493cbeb9865ad9d02287ed07
Zhesha <aleshko.info@gmail.com>
2023-04-17 20:35:14 +0400

    finished tree's binary

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,57 +7,145 @@ const { Node } = require('../extensions/list-tree.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-
   constructor(data){
-    this.rootBereza = null;
-
+    this.basis = null;
   }
 
   root() {
-    //throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    return this.root;
+    return this.basis;
   }
 
   add(data) {
-    //throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    if(this.rootBereza === null){
-      this.rootBereza === data;
-      return;
+    this.basis = putInside(this.basis, data);
+
+    function putInside(param, data){
+      if(param == undefined){
+        return new Node(data);
+      }
+
+      if(param.data === data){
+        return param;
+      }
+
+      if(data < param.data){
+        param.left = putInside(param.left, data);
+      }else{
+        param.right = putInside(param.right, data);
+      }
+
+      return param;
     }
 
-    
-
-
-
-    
-    
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return putInside(this.basis, data);
+
+    function putInside(param,data){
+      if(param == undefined){
+        return false;
+      }
+
+      if(param.data === data){
+        return true;
+      }
+
+      return data < param.data ?
+        putInside(param.left, data):
+        putInside(param.right,data);
+    }
+
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findParamNode(this.basis, data);
+
+    function findParamNode(param,data){
+      if(param == undefined){
+        return null;
+      }
+
+      if(data < param.data){
+        return findParamNode(param.left, data);
+      }
+
+      if(data > param.data){
+        return findParamNode(param.right, data);
+      }
+
+      return param;
+      
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.basis = removeParam(this.basis,data);
+
+    function removeParam(param,data){
+      if(param == undefined){
+        return null;
+      }
+
+      if(data < param.data){
+        param.left = removeParam(param.left,data);
+        return param;
+      }else if(param.data < data){
+        param.right = removeParam(param.right,data);
+        return param;
+      }else{
+        if(param.left == undefined && param.right == undefined){
+          return null;
+        }
+
+        if(param.left == undefined){
+          param = param.right;
+          return param;
+        }
+
+        if(param.right == undefined){
+          param = param.left;
+          return param;
+        }
+
+        let minFromRight = param.right;
+        while(minFromRight.left){
+          minFromRight = minFromRight.left;
+        }
+
+        param.data = minFromRight.data;
+
+        param.right = removeParam(param.right,minFromRight.data);
+
+        return param;
+      }
+    }
+    
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.basis == undefined){
+      return;
+    }
+
+    let param = this.basis;
+    while(param.left){
+      param = param.left;
+    }
+
+    return param.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.basis == undefined){
+      return;
+    }
+
+    let param = this.basis;
+    while(param.right){
+      param = param.right;
+    }
+
+    return param.data;
   }
 }
 
